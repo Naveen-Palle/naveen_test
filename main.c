4359d99991d1d81f545aaf9fb62e73cefd9b9563
Naveen-Palle <111974174+Naveen-Palle@users.noreply.github.com>
2022-08-25 17:53:12 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int c;
+    int d;
+    printf("enter two number: ");
+    scanf("%d %d",&a,&c);
+    d=a+c;
+    printf("sum of two is : %d",d);
+    d=0;
+    d=a-c;
+     printf("\nsum of sub is : %d",d);
+     d=0;
+    d=a/c;
+     printf("\nsum of div is : %d",d);
+    
+}
